ce11c32eca94a32e90f8d67bc828e5a4ff50d19f
Gabriel Bittencourt Camillo de Souza <86302613+GabrielBitten@users.noreply.github.com>
2024-11-10 11:45:04 -0300

    Update ia.py

--- IA files/ia.py
+++ IA files/ia.py
@@ -1,35 +1,26 @@
-import pandas as pd
+import pandas as pandas_lib
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-
-
-data = pd.read_csv('data.csv', header=None, names=['senha', 'força'])  
-X = data['senha']
-y = data['força']
-
-
-vectorizer = CountVectorizer(analyzer='char', ngram_range=(2, 4))
-X = vectorizer.fit_transform(X)
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
-model = RandomForestClassifier()
-model.fit(X_train, y_train)
-
-
-y_pred = model.predict(X_test)
-
-
-print(accuracy_score(y_test, y_pred))
-
 import pickle
 
+senha_data = pandas_lib.read_csv('data.csv', header=None, names=['senha', 'forca'])  
+senhas = senha_data['senha']
+forca = senha_data['forca']
 
-with open('password_strength_model.pkl', 'wb') as model_file:
-    pickle.dump(model, model_file)
-with open('vectorizer.pkl', 'wb') as vectorizer_file:
-    pickle.dump(vectorizer, vectorizer_file)
+senha_vectorizer = CountVectorizer(analyzer='char', ngram_range=(2, 4))
+senhas_vetorizadas = senha_vectorizer.fit_transform(senhas)
+
+X_train, X_test, y_train, y_test = train_test_split(senhas_vetorizadas, forca, test_size=0.2, random_state=42)
+
+classificador_forca_senha = RandomForestClassifier()
+classificador_forca_senha.fit(X_train, y_train)
+
+predicoes_forca = classificador_forca_senha.predict(X_test)
+print(accuracy_score(y_test, predicoes_forca))
+
+with open('password_strength_model.pkl', 'wb') as arquivo_modelo:
+    pickle.dump(classificador_forca_senha, arquivo_modelo)
+with open('vectorizer.pkl', 'wb') as arquivo_vectorizer:
+    pickle.dump(senha_vectorizer, arquivo_vectorizer)
